1f4fe4383df71f4062dc728d34cb21d04136ef1b
AlexanderJGu <gu.alex.88@gmail.com>
2024-06-03 15:24:50 -0700

    Fill in details

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import Image from "next/image";
-import image_src from './images/logo.svg'
+import image_src from './images/logo.png'
 
 const config: DocsThemeConfig = {
   logo: (
-    <Image src={image_src} alt={""} width={100}/>
+    <Image src={image_src} alt={""} width={160}/>
   ),
   nextThemes: { defaultTheme: "light", forcedTheme: "light" },
   useNextSeoProps() {
     return {
-      titleTemplate: "%s - [todo] Testnet Docs",
+      titleTemplate: "%s - ApeChain Docs",
     };
   },
   navigation: false,
