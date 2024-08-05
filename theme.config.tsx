6beaadcb64a8be60ac6127da50c706314c269ec5
robbiekruszynski <robbiekruszynski@gmail..com>
2024-08-05 15:10:08 +0200

    br branch push

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import Image from "next/image";
-import image_src from './images/logo.png'
+import image_src from './images/Group.png'
 
 const config: DocsThemeConfig = {
   logo: (
-    <Image src={image_src} alt={""} width={160}/>
+    <Image src={image_src} alt={""} width={
+      60}/>
   ),
   nextThemes: { defaultTheme: "dark" },
   useNextSeoProps() {
