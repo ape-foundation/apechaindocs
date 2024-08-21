5ba355e86dda8ad9e6212dff6ca8f1c48cbe492f
Robbiekruszynski <robbie@apecoin.com>
2024-08-21 18:33:42 +0200

    test adj on theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,32 +1,34 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import Image from "next/image";
-import image_src from './images/Group.png'
-import { ThemeSwitch } from './components/ThemeSelector'
-import { AskCookbook } from './components/AskCookbook'
+import image_src from './images/Group.png';
+import { ThemeSwitch } from './components/ThemeSelector';
+import { AskCookbook } from './components/AskCookbook';
 
 const config: DocsThemeConfig = {
   logo: (
-    <Image src={image_src} alt={""} width={
-      60}/>
+    <Image src={image_src} alt={""} width={60} />
   ),
   nextThemes: { defaultTheme: "dark" },
   useNextSeoProps() {
     return {
       titleTemplate: "%s - ApeChain Docs",
+      defaultTitle: "ApeChain Docs",
+      description: "Official ApeChain Documentation",
     };
   },
   navigation: false,
   themeSwitch: {
-    component: <ThemeSwitch />
+    component: <ThemeSwitch />,
   },
   navbar: {
     extraContent: (
-      <div >
+      <div>
         <AskCookbook />
       </div>
     ),
-  }
+  },
 };
 
 export default config;
+
