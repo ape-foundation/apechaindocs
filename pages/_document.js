444366837c9ce11ff5fdb30db4e3439411f8b107
Robbiekruszynski <robbie@apecoin.com>
2024-08-22 12:27:22 +0200

    fav add with _doc

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+      
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta name="ApeChain docs" content="ApeChain Documentation" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
