a2e5f011b3aa0dd7c2b0a42e4a634aa8fd0d708f
Robbiekruszynski <robbie@apecoin.com>
2024-08-27 22:24:43 +0200

    update hh again

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import Head from 'next/head';
 import Image from "next/image";
 import logo_image_src from './images/apechain-logo.svg';
 import og_image_src from './images/apechain-open-graph.jpg';
@@ -46,6 +47,17 @@ const config: DocsThemeConfig = {
       </>
     ),
   },
+  head: (
+    <Head>
+      <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism.css"
+        rel="stylesheet"
+      />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/prism.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/plugins/toolbar/prism-toolbar.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js"></script>
+    </Head>
+  ),
 };
 
 export default config;
